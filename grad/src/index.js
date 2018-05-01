baa879e1e9322af8bbb224f85a128fd124d53fd7
RiStar <ristar@rentianxingdeMacBook-Pro.local>
2018-05-02 01:10:38 +0800

    first pro

--- grad/src/index.js
+++ grad/src/index.js
@@ -1,14 +1,17 @@
 import dva from 'dva';
 import './index.css';
+// import browserHistory from 'history/createBrowserHistory';
 
 // 1. Initialize
-const app = dva();
+const app = dva({
+    // history: browserHistory(),
+});
 
 // 2. Plugins
 // app.use({});
 
 // 3. Model
-// app.model(require('./models/example').default);
+app.model(require('./models/example').default);
 
 // 4. Router
 app.router(require('./router').default);
